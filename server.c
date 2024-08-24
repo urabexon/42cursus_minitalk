3bc2a6eebbcd95ab22b61afb9f25a35e9054e3ea
Hiroki Urabe <hurabe@c3r4s3.42tokyo.jp>
2024-08-24 21:04:25 +0900

    add error

--- server.c
+++ server.c
@@ -6,7 +6,24 @@
 /*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 16:36:20 by hurabe            #+#    #+#             */
-/*   Updated: 2024/08/19 16:36:21 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/08/24 20:57:43 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+void	ft_error(int error)
+{
+	if (error == INPUT_ERROR)
+		ft_printf("'./client [PID] [string]'");
+	if (error == PID_ERROR)
+		ft_printf("PID is wrong, Please check again PID!");
+	if (error == KILL_ERROR)
+		ft_printf("kell command error");
+	exit(EXIT_FAILURE);
+}
+
+int	main(void)
+{
+	return (0);
+}
